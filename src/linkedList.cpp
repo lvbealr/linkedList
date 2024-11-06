311134fab10ce1aad58b932cdc00e65fd3afe738
lvbealr <lvbealr@mail.ru>
2024-11-06 22:15:49 +0300

    clear code, func to fill data with poison, getNodeValue func

--- src/linkedList.cpp
+++ src/linkedList.cpp
@@ -5,36 +5,52 @@
 #include "linkedList.h"
 #include "customWarning.h"
 
-// TODO VERIFY IN EVERY FUNCTION
+#define CHECK_ERROR(listPtr, expression, error) { \
+  if (!(expression)) {                            \
+    (listPtr)->errorCode |= error;                \
+    return error;                                 \
+  }                                               \
+}
+
+static linkedListError fillWithPoison(linkedList *list) {
+  customWarning(list                  != NULL, LIST_BAD_POINTER);
+
+  CHECK_ERROR(list, sizeof(list->data[0]) == sizeof(POISON_VALUE), BAD_POISON_SIZE);
+
+  for (size_t index = 0; index < list->capacity; index++) {
+    list->data[index] = POISON_VALUE;
+  }
+
+  linkedListVerify(list);
+
+  return NO_ERRORS;
+}
 
 linkedListError linkedListInitialize(linkedList *list, size_t capacity) {
   customWarning(list != NULL, LIST_BAD_POINTER);
 
   list->capacity = (ssize_t)capacity + 1;
 
-  // list->data     = (elem_t  *)customCalloc((size_t)list->capacity, sizeof(elem_t), 666);
   list->data     = (elem_t  *)calloc((size_t)list->capacity, sizeof(elem_t));
   list->prev     = (ssize_t *)calloc((size_t)list->capacity, sizeof(ssize_t));
   list->next     = (ssize_t *)calloc((size_t)list->capacity, sizeof(ssize_t));
 
-  customWarning(list->data != NULL, DATA_BAD_POINTER);
-  customWarning(list->prev != NULL, PREVIOUS_BAD_POINTER); // TODO MAYBE RENAME ENUM
-  customWarning(list->next != NULL, NEXT_BAD_POINTER);     // TODO MAYBE RENAME ENUM
+  CHECK_ERROR(list, list->data != NULL,     DATA_BAD_POINTER);
+  CHECK_ERROR(list, list->prev != NULL, PREVIOUS_BAD_POINTER);
+  CHECK_ERROR(list, list->next != NULL,     NEXT_BAD_POINTER);
 
-  list->data[0]  = 0; // TODO all cells to poison
-  list->prev[0]  = 0; // ПУСТАЯ ЯЧЕЙКА, НИКУДА НЕ УКАЗЫВАЕТ
-  list->next[0]  = 0; // ПУСТАЯ ЯЧЕЙКА, НИКУДА НЕ УКАЗЫВАЕТ
-  list->freeNode = 1; // В НУЛЕВУЮ ЯЧЕЙКУ НИЧЕГО НЕ КЛАДЕМ, ВСЕ УЗЛЫ СЕЙЧАС СВОБОДНЫ, НАЧИНАЕМ С ПЕРВОГО РАССТАВЛЯТЬ ИНДЕКСЫ
+  fillWithPoison(list);
+
+  list->prev[0]  = 0;
+  list->next[0]  = 0;
+  list->freeNode = 1;
 
   for (ssize_t nodeIndex = list->freeNode; nodeIndex < list->capacity; nodeIndex++) {
-    list->next[nodeIndex] = (nodeIndex + 1) % list->capacity; // КОЛЬЦЕВАЯ ИНДЕКСАЦИЯ
-    list->prev[nodeIndex] = -1;                               // НЕВАЛИДНЫЙ ПОИНТЕР
+    list->next[nodeIndex] = (nodeIndex + 1) % list->capacity;
+    list->prev[nodeIndex] = -1;
   }
 
-  // for (ssize_t nodeIndex = list->freeNode; nodeIndex < list->capacity; nodeIndex++) {
-  //   printf("[%d]->%d ", nodeIndex, list->next[nodeIndex]);
-  // }
-  // printf("\n");
+  linkedListVerify(list);
 
   return NO_ERRORS;
 }
@@ -44,27 +60,39 @@ linkedListError linkedListDestruct(linkedList *list) {
 
   list->capacity = -1;
   list->freeNode = -1;
+  list->errorCode = 0;
 
   FREE_(list->data);
   FREE_(list->prev);
   FREE_(list->next);
 
+  linkedListVerify(list);
+
   return NO_ERRORS;
 }
 
 linkedListError linkedListVerify    (linkedList *list) {
-  // TODO do list with errors like in stack instead of custom warnings
-  customWarning(list           != NULL,                                LIST_BAD_POINTER    );
-  customWarning(list->data     != NULL,                                DATA_BAD_POINTER    );
-  customWarning(list->prev     != NULL,                                PREVIOUS_BAD_POINTER);
-  customWarning(list->next     != NULL,                                NEXT_BAD_POINTER    );
-  customWarning(list->capacity < 0,                                    BAD_CAPACITY        );
-  customWarning(list->prev[0]  < 0 || list->prev[0] >= list->capacity, BAD_TAIL            );
-  customWarning(list->next[0]  < 0 || list->prev[0] >= list->capacity, BAD_HEAD            );
+  customWarning(list                  !=                                NULL, LIST_BAD_POINTER    );
 
-  // TODO CHECK freeNode?
+  CHECK_ERROR   (list, list->data     !=                                NULL, DATA_BAD_POINTER    );
+  CHECK_ERROR   (list, list->prev     !=                                NULL, PREVIOUS_BAD_POINTER);
+  CHECK_ERROR   (list, list->next     !=                                NULL, NEXT_BAD_POINTER    );
+  CHECK_ERROR   (list, list->capacity >=                                   0, BAD_CAPACITY        );
+  CHECK_ERROR   (list, list->next[0]  >= 0 && list->next[0] < list->capacity, BAD_HEAD            );
+  CHECK_ERROR   (list, list->prev[0]  >= 0 && list->prev[0] < list->capacity, BAD_TAIL            );
 
-  // TODO MORE CHECKS
+  ssize_t freeIndex = list->freeNode;
+
+  while (freeIndex) {
+    if (list->prev[freeIndex] <= 0) {
+      list->errorCode |= BAD_FREE_NODE;
+      return BAD_FREE_NODE;
+    }
+
+    else {
+      freeIndex = list->next[freeIndex];
+    }
+  }
 
   return NO_ERRORS;
 }
@@ -76,54 +104,58 @@ linkedListError linkedListDump      (linkedList *list) {
 
 linkedListError insertNode          (linkedList *list, ssize_t index, ssize_t *newIndex, elem_t value) {
   customWarning(list != NULL, NODE_BAD_POINTER);
-  // TODO CHECK INDEX, CAPACITY
 
-  *newIndex                     = list->freeNode;             // newIndex - индекс свободной (freeNode) ячейки
-  list->freeNode                = list->next[list->freeNode]; // свободной становится ячейка, следующая от предыдущей свободной
+  CHECK_ERROR(list, index             >=  0 && index < list->capacity,    BAD_INDEX);
+  CHECK_ERROR(list, list->prev[index] !=                           -1,    BAD_INDEX);
+  CHECK_ERROR(list, list->freeNode    !=                            0, BAD_CAPACITY);
 
-  list->data[*newIndex]         = value;                      // в новую (свободную) ячейку кладем значение
+  *newIndex                     = list->freeNode;
+  list->freeNode                = list->next[list->freeNode];
+  list->data[*newIndex]         = value;
+  list->prev[list->next[index]] = *newIndex;
+  list->prev[*newIndex]         = index;
 
-  list->prev[list->next[index]] = *newIndex;                  // для следующей от старой ячейки, указателем на предыдущую становится новый индекс
-  list->prev[*newIndex]         = index;                      // для новой ячейки предыдущая ячейка - та, после которой вставляем,
-                                                              // то есть с индексом index
+  list->next[*newIndex]         = list->next[index];
 
-  list->next[*newIndex]         = list->next[index];          // следующей для новой ячейки становится указатель на следующую для старой
-                                                              // (просто поместили между ними)
-  list->next[index]             = *newIndex;                  // новая ячейка встала справа от старой, значит для старойуказатель на след.
-                                                              // - указатель на новую
+  list->next[index]             = *newIndex;
+
+  linkedListVerify(list);
 
   return NO_ERRORS;
 }
 
 linkedListError deleteNode          (linkedList *list, ssize_t index) {
   customWarning(list != NULL, LIST_BAD_POINTER);
-  // TODO CHECKER
 
-  list->prev[list->next[index]] = list->prev[index]; // [A -> B -> C], del B, [A -> C], для С предыдущей становится предыдущая для В, т. е. А
-  list->next[list->prev[index]] = list->next[index]; // [A -> B -> C], del B, [A -> C], для А следующей  становится следующая  для В, т. е. С
+  CHECK_ERROR(list, index > 0 && index < list->capacity, BAD_INDEX);
+  CHECK_ERROR(list, list->prev[index] !=             -1, BAD_INDEX);
 
-  list->next[index] = list->freeNode;                // [A -> ... -> С]
-  list->prev[index] = -1;                            // эмэаээаэаэа надо подумать поч невалидный поинтер на предыдущую 🤔🤔🤔🤔🤔
-  list->freeNode    = index;                         // ячейка, которую занимала В, теперь свободна
-  // TODO это как будто не совсем правильно, наверное узлы должны сдвигаться друг к другу? так между ними пустые узлы
-  // UPD: нет вроде нормально
+  list->data[index] = POISON_VALUE;
+
+  list->prev[list->next[index]] = list->prev[index];
+  list->next[list->prev[index]] = list->next[index];
+
+  list->next[index] = list->freeNode;
+  list->prev[index] = -1;
+  list->freeNode    = index;
+
+  linkedListVerify(list);
 
   return NO_ERRORS;
 }
 
-// TODO подумать как лучше это реализовать? сделать отдельную структурку, чтобы вытаскивать целую структуру с next, prev, value
-// TODO или достаточно доставать только значение по индексу например (чекнуть дефолтные функции на cppreference)
+linkedListError getNodeValue         (linkedList *list, ssize_t index, elem_t *value) {
+  customWarning(list != NULL, LIST_BAD_POINTER);
 
-// linkedListError getNode             (linkedList *list, linkedListNode *getNodeInfo, size_t index) {
-//   customWarning(list != NULL, LIST_BAD_POINTER);
-//   // TODO CHECK FOR GET NODE INFO PTR
+  CHECK_ERROR  (list, index >= 0 && index < list->capacity,            BAD_INDEX);
+  CHECK_ERROR  (list, value !=                        NULL, BAD_GET_NODE_POINTER);
 
-//   getNodeInfo->value = list->data[index];
-//   getNodeInfo->prev  = list->prev[index];
-//   getNodeInfo->next  = list->next[index];
+  *value = list->data[index];
 
-//   return NO_ERRORS;
-// }
+  linkedListVerify(list);
+
+  return NO_ERRORS;
+}
 
 
 
