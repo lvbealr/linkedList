9d15d6af7a98dc5ae8dd9e13631a5d0d609dc3ac
lvbealr <lvbealr@mail.ru>
2024-11-06 22:15:19 +0300

    getNodeValue func and some new enums

--- include/linkedList.h
+++ include/linkedList.h
@@ -2,6 +2,7 @@
 #define LINKED_LIST_H_
 
 #include <cstdlib>
+#include <cinttypes>
 
 typedef int elem_t;
 
@@ -18,21 +19,24 @@ struct linkedList {
   ssize_t        *next             = {};
   ssize_t         capacity         = -1;
   ssize_t         freeNode         = -1;
+  uint64_t        errorCode        =  0;
 };
 
-// TODO ENUM
-
 enum linkedListError {
-  NO_ERRORS            = 0,
-  LIST_BAD_POINTER     = 1,
-  PREVIOUS_BAD_POINTER = 2,
-  NEXT_BAD_POINTER     = 3,
-  DATA_BAD_POINTER     = 4,
-  BAD_CAPACITY         = 5,
-  BAD_HEAD             = 6,
-  BAD_TAIL             = 7,
-  NODE_BAD_POINTER     = 8,
-  ROOT_BAD_POINTER     = 9
+  NO_ERRORS            =       0,
+  LIST_BAD_POINTER     = 1 <<  1,
+  PREVIOUS_BAD_POINTER = 1 <<  2,
+  NEXT_BAD_POINTER     = 1 <<  3,
+  DATA_BAD_POINTER     = 1 <<  4,
+  BAD_CAPACITY         = 1 <<  5,
+  BAD_HEAD             = 1 <<  6,
+  BAD_TAIL             = 1 <<  7,
+  NODE_BAD_POINTER     = 1 <<  8,
+  ROOT_BAD_POINTER     = 1 <<  9,
+  BAD_POISON_SIZE      = 1 << 10,
+  BAD_FREE_NODE        = 1 << 11,
+  BAD_INDEX            = 1 << 12,
+  BAD_GET_NODE_POINTER = 1 << 13
 };
 
 // FUNCTION PROTOTYPES //
@@ -42,7 +46,7 @@ linkedListError linkedListVerify    (linkedList     *list                       
 linkedListError linkedListDump      (linkedList     *list                                               );
 linkedListError insertNode          (linkedList     *list, ssize_t index, ssize_t *newIndex, elem_t data);
 linkedListError deleteNode          (linkedList     *list, ssize_t index                                );
-// linkedListError getNode             (linkedList     *list                              );
+linkedListError getNodeValue        (linkedList     *list, ssize_t index, elem_t *value                 );
 // FUNCTION PROTOTYPES //
 
 #endif // LINKED_LIST_H_
